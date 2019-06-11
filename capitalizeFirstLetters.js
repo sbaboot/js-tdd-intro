4b04fd2d6edf8f1b663649b24d984ec2fa5616e5
sbaboot <sebastienflamant31@gmail.com>
2019-06-11 11:00:47 +0200

    tests et fonction OK

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,19 +1,26 @@
-// capitalizeFirst.js
+// capitalizeFirstLetters.js
 const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
-function capitalizeFirst(input) {
-  return input.length > 0
-    ? input[0].toUpperCase() + input.slice(1)
-    : '';
+function capitalizeFirstLetters(input) {
+  if (input === '') {
+    return ''
+  } else {
+    return input
+      .toLowerCase()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
 }
+
 // Check that capitalizeFirst is a function
-assert.strictEqual(typeof capitalizeFirst, 'function');
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 // Check that capitalizeFirst accepts one argument
-assert.strictEqual(capitalizeFirst.length, 1);
+assert.strictEqual(capitalizeFirstLetters.length, 1);
 // Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+assert.strictEqual(capitalizeFirstLetters('javascript is good'), 'Javascript Is Good');
 // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetters('zorro'), 'Zorro');
 // Check that it works for an empty string
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters(''), '');
